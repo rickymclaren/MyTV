c1e08fd9247b80c02c22b3f68f110f437e27026c
Ricky McLaren <ricky.mclaren@cybg.com>
2017-02-07 23:25:10 +0000

    Latest links

---
+++ edit.sh
@@ -0,0 +1,2 @@
+vim scp://osmc@192.168.0.5/.kodi/addons/plugin.video.mytv/default.py
+
--- plugin.video.mytv/default.py
+++ plugin.video.mytv/default.py
@@ -1,15 +1,16 @@
 import xbmcaddon, util
 
 channels = {
-    '1 TVE1' : 'http://hlsackdn_gl7-lh.akamaihd.net/i/hlslive_1@439316/master.m3u8',
-    '2 TVE2' : 'http://rtve-live.hds.adaptive.level3.net/hls-live/rtvegl0-la2lv3aomgl0/_definst_/live.m3u8',
-    '3 Antena3' : 'http://a3live-lh.akamaihd.net/i/antena3_1@35248/index_1_av-p.m3u8',
-    '4 Cuatro' : 'http://mdsiosgeo2-lh.akamaihd.net/i/mitele/esmediaset_22@168498/index_0_av-b.m3u8',
-    '5 Cinco' : 'http://mdsios1-lh.akamaihd.net/i/mitele/esmediaset_11@168471/index_0_av-b.m3u8',
-    '6 La Sexta' : 'http://a3live-lh.akamaihd.net/i/lasexta_1@35272/master.m3u8',
-    '7 24H' : 'http://rtve-live.hds.adaptive.level3.net/hls-live/rtvegl8-24hlv3aomgl8/_definst_/live.m3u8',
-    '8 TDP' : 'http://hlsackdn_gl8-lh.akamaihd.net/i/hlslive_1@441481/master.m3u8',
-    '9 Test' : 'http://rtve-live.hds.adaptive.level3.net/hls-live/rtvegl7-la1lv3aomgl7/_definst_/live.m3u8',
+    '01 TVE1' : 'http://rtve-live.hds.adaptive.level3.net/hls-live/rtvegl7-la1lv3aomgl7/_definst_/live.m3u8',
+    '02 TVE2' : 'http://rtve-live.hds.adaptive.level3.net/hls-live/rtvegl0-la2lv3aomgl0/_definst_/live.m3u8',
+    '03 Antena3' : 'http://a3live-lh.akamaihd.net/i/antena3_1@35248/index_1_av-p.m3u8',
+    '04 Cuatro' : 'http://mdsiosgeo2-lh.akamaihd.net/i/mitele/esmediaset_22@168498/index_0_av-b.m3u8',
+    '05 Cinco' : 'http://mdsios1-lh.akamaihd.net/i/mitele/esmediaset_11@168471/index_0_av-b.m3u8',
+    '06 La Sexta' : 'http://a3live-lh.akamaihd.net/i/lasexta_1@35272/master.m3u8',
+    '07 24H' : 'http://rtve-live.hds.adaptive.level3.net/hls-live/rtvegl8-24hlv3aomgl8/_definst_/live.m3u8',
+    '08 TDP' : 'http://hlsackdn_gl8-lh.akamaihd.net/i/hlslive_1@441481/master.m3u8',
+    '09 Cataluna' : 'http://rtve-live.hds.adaptive.level3.net/hls-live/rtvegl7-la1calv3aomgl7/_definst_/live.m3u8',
+    '10 Test' : 'http://hlsackdn_gl7-lh.akamaihd.net/i/hlslive_1@439315/master.m3u8',
 
 }
 
