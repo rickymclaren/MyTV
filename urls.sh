d2f3a685d8a623cad5bf54928616af06eb23e7c7
Ricky McLaren <ricky.mclaren@cybg.com>
2017-06-15 00:34:02 +0100

    Use youtube-dl

---
+++ urls.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+youtube-dl -g http://www.rtve.es/directo/la-1/
+youtube-dl -g http://www.rtve.es/directo/la-2/
+youtube-dl -g http://www.rtve.es/directo/teledeporte/
+youtube-dl -g http://www.rtve.es/directo/canal-24h/
+
+
