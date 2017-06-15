049d645d18a5256ee9e9e6b5b18f2a5c9961e549
Ricky McLaren <ricky.mclaren@cybg.com>
2017-06-15 01:01:00 +0100

    Only call youtube-dl when playing url

--- plugin.video.mytv/default.py
+++ plugin.video.mytv/default.py
@@ -1,14 +1,14 @@
 import xbmcaddon, util, commands
 
 channels = {
-    '01 TVE1' : commands.getoutput('youtube-dl -g http://www.rtve.es/directo/la-1/').split()[-1],
-    '02 TVE2' : commands.getoutput('youtube-dl -g http://www.rtve.es/directo/la-2/').split()[-1],
+    '01 TVE1' : 'url: http://www.rtve.es/directo/la-1/',
+    '02 TVE2' : 'url: http://www.rtve.es/directo/la-2/',
     '03 Antena3' : 'http://a3live-lh.akamaihd.net/i/antena3_1@35248/index_1_av-p.m3u8',
     '04 Cuatro' : 'http://mdsiosgeo2-lh.akamaihd.net/i/mitele/esmediaset_22@168498/index_0_av-b.m3u8',
     '05 Cinco' : 'http://mdsios1-lh.akamaihd.net/i/mitele/esmediaset_11@168471/index_0_av-b.m3u8',
     '06 La Sexta' : 'http://a3live-lh.akamaihd.net/i/lasexta_1@35272/master.m3u8',
-    '07 24H' : commands.getoutput('youtube-dl -g http://www.rtve.es/directo/canal-24h/').split()[-1],
-    '08 TDP' : commands.getoutput('youtube-dl -g http://www.rtve.es/directo/teledeporte/').split()[-1],
+    '07 24H' : 'url: http://www.rtve.es/directo/canal-24h/',
+    '08 TDP' : 'url: http://www.rtve.es/directo/teledeporte/',
     '09 Cataluna' : 'http://rtve-live.hds.adaptive.level3.net/hls-live/rtvegl7-la1calv3aomgl7/_definst_/live.m3u8',
 }
 
@@ -16,7 +16,11 @@ channels = {
 def playVideo(params):
     #util.notify('plugin.video.mytv', "playing video %r" % params['channel'])
     addon = xbmcaddon.Addon('plugin.video.mytv')
-    util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), channels[params['channel']] )
+    channel = channels[params['channel']]
+    if channel.startswith('url:'):
+        channel = channel.split()[-1]
+        channel = commands.getoutput('youtube-dl -g %s' % channel).split()[-1]
+    util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), channel )
 
 def buildMenu():
     for channel in sorted(channels.keys()):
