ac2942363f1adbaa2471462059bd825c0f4dc818
Ricky McLaren <ricky.mclaren@cybg.com>
2018-06-12 11:25:18 +0100

    Antena 3

--- plugin.video.mytv/default.py
+++ plugin.video.mytv/default.py
@@ -7,7 +7,7 @@ import xbmcaddon, util, commands
 channels = {
     '01 TVE1' : 'url: http://www.rtve.es/directo/la-1/',
     '02 TVE2' : 'url: http://www.rtve.es/directo/la-2/',
-    '03 Antena3' : 'http://a3live-lh.akamaihd.net/i/antena3_1@35248/index_1_av-p.m3u8',
+    '03 Antena3' : 'https://atresplayerp-i.akamaized.net/antena3mpp/master.m3u8',
     '04 Cuatro' : 'http://mdsiosgeo2-lh.akamaihd.net/i/mitele/esmediaset_22@168498/index_0_av-b.m3u8',
     '05 Cinco' : 'http://mdsios1-lh.akamaihd.net/i/mitele/esmediaset_11@168471/index_0_av-b.m3u8',
     '06 La Sexta' : 'http://a3live-lh.akamaihd.net/i/lasexta_1@35272/master.m3u8',
