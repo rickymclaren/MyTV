ac27694e890e536134505838f1e645eb22c8be94
Ricky McLaren <ricky.mclaren@cybg.com>
2020-04-11 21:26:56 +0100

    Add channels

--- urls.sh
+++ urls.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
 
+echo '#EXTINF:-1,LA-1'
 youtube-dl -g http://www.rtve.es/directo/la-1/
+echo '#EXTINF:-1,LA-2'
 youtube-dl -g http://www.rtve.es/directo/la-2/
+echo '#EXTINF:-1,Teledeporte'
 youtube-dl -g http://www.rtve.es/directo/teledeporte/
+echo '#EXTINF:-1,Canal 24H'
 youtube-dl -g http://www.rtve.es/directo/canal-24h/
 
 
